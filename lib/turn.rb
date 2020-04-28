fe621a0cb0ee01d60cd5a8a674285aac0838f1b0
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-28 13:50:06 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+def input_to_index(user_input)
+  converted_input = user_input.to_i
+  converted_input = converted_input - 1
+  return converted_input
+end
+def move(board, index, character = "X")
+  board[index] = character
+end
+
+
+
+def position_taken?(board, index)
+if board[index] == " " || board[index] == "" || board[index] == nil 
+  return false
+else
+  return true
+end
+end

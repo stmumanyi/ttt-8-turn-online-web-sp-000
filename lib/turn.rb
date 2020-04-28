1908b5b2a9649976182888f009a730220110c6c2
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-28 13:59:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,6 +38,6 @@ def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
   index = input_to_index(num)
-  if valid_move?(board,index) == true
-    move(board)
+  if valid_move?(board, index) == true
+    move(board, index)
 end

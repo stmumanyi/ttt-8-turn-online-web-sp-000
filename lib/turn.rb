991c41220bcd744d6725a6403359378fd010dce2
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-28 13:50:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,20 @@ end
 def move(board, index, character = "X")
   board[index] = character
 end
+def valid_move?(board, index)
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+  return false
+  else
+  return true
+  end
+  end 
+  def within_board(index)
+  if index.between?(0,8) == true
+    return true
+  else
+    false
+  end
+  end
+  if position_taken?(board, index) == false && within_board(index) == true
+    return true
